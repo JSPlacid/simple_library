a4f2c6ce02562558daf49f078e8ba6b9758175d5
unknown <jspacific777@gmail.com>
2023-06-07 16:45:20 +0100

    First version of project app moved to remote

---
+++ simplelibrary/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for simplelibrary project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplelibrary.settings')
+
+application = get_asgi_application()
